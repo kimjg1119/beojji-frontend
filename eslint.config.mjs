f1eac4c603c9af13fae0ceb150e3d4241d2943a1
Jungyeom Kim <kimjg1199lock@gmail.com>
2024-09-08 20:50:18 +0900

    Why it fails

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,10 @@ export default [
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     }
   }
 ];
